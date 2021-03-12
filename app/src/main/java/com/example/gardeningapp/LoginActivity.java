947c38a73bb0e6b17c47b37b95922216b853a090
Mayukh Mitra <mayukhmitra941@gmail.com>
2021-03-12 14:22:31 +0530

    Gardening app 1.0

---
+++ app/src/main/java/com/example/gardeningapp/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.gardeningapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button button=findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                Toast.makeText(getApplicationContext(),"Logged in successfully",Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+    }
+}
